61f2a1e847e005521b6913144a93cba86a0b6e4b
jhell85 <jhell85@gmail.com>
2021-02-11 12:09:54 -0800

    add new portfolio site set up

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { createRef } from "react";
+import { Image, Input, Menu, Segment, Sticky } from "semantic-ui-react";
+import _ from "lodash";
+import landingImage from "./img/landing2.jpg";
+import "./App.css";
+import FakeData from "./fake-data";
+import ScrollTo from "react-scroll-into-view";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  contextRef = createRef();
+
+  render() {
+    return (
+      <div ref={this.contextRef}>
+        <Image className="landingImg" src={landingImage} />
+        <Sticky context={this.contextRef}>
+          <Menu color="blue" widths={3}>
+            <ScrollTo className="menu item" selector="#about-content">
+              About
+            </ScrollTo>
+            <ScrollTo className="menu item" selector="#portfolio-content">
+              Portfolio
+            </ScrollTo>
+            <ScrollTo className="menu item" selector="#contact-content">
+              Contact
+            </ScrollTo>
+          </Menu>
+        </Sticky>
+        <Segment attached="bottom">
+          <div id="about-content">
+            <h5>About</h5>
+            {_.times(10, (i) => (
+              <Image
+                key={i}
+                src="https://react.semantic-ui.com/images/wireframe/paragraph.png"
+              />
+            ))}
+          </div>
+          <div id="portfolio-content">
+            <h5>Portfolio</h5>
+            <FakeData />
+          </div>
+          <div id="contact-content">
+            <h5>contact</h5>
+            <FakeData />
+          </div>
+        </Segment>
+      </div>
+    );
+  }
 }
 
 export default App;
