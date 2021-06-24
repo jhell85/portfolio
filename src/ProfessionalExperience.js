b750037d9190977bee0ed3c884b059041b6b74d1
jhell85 <jhell85@gmail.com>
2021-06-24 08:51:46 -0700

    add code guild to experiance removed inline styles and updated other imgs css

--- src/ProfessionalExperience.js
+++ src/ProfessionalExperience.js
@@ -8,25 +8,54 @@ const ProfessionalExperience = () => {
       <Container textAlign="center">
         <h2>Professional Experience</h2>
       </Container>
-      <div className="ui items">
-      <div className="item">
-          <div style={{background: 'white', minHeight: '100px'}} className="image">
-            <img style={{top: "50%", transform: "translateY(50%)", padding:"5px", justifyContent: 'center' }} 
+      <div className="ui unstackable items">
+
+        <div className="item">
+          <div className="ui small experience-img image">
+            <img 
+            src="https://pdxcodeguild.com/static/img/pdxcglogo.svg" 
+            alt="PDX Code Guild logo" />
+          </div>
+          <div className="middle aligned content" >
+            <StyledA
+              target="_blank"
+              rel="noopener noreferrer"
+              href="https://pdxcodeguild.com/"
+              className="header"
+            >
+              PDX Code Guild
+            </StyledA>
+            <div class="ui header padding-left">May 2021 - present</div>
+            <div class="description">
+              <p>
+                Working as a Teacher's assistant helping in the Code Guild's Fullstack web development course. Assisting students in learning
+                to programing in Python, JavaScript, HTML & CSS languages, and helping students develop full web applications using Python's 
+                Django Framework.  
+              </p>
+            </div>
+          </div>
+        </div>
+
+        <div className="item">
+          <div className="ui small image experience-img rpc">
+            <img
             src="https://s3-us-west-2.amazonaws.com/freddieflip/uploads/2018/08/06053351/RPC-Logo_Primary-Logo1.png" 
             alt="Rock Paper Coin" />
           </div>
-          <div className="content" style={{ paddingTop: "10px" }}>
+          <div className="middle aligned content">
             <StyledA
               target="_blank"
               rel="noopener noreferrer"
               href="https://rockpapercoin.com/"
-              className="header"
+              className="ui header"
             >
               Rock Paper Coin
             </StyledA>
-            <div style={{ paddingLeft: "5px" }} class="ui header">March 2021 - May 2021</div>
-            <div class="description">
-              <p>
+            <div class="ui header padding-left">March 2021 - May 2021</div>
+            <div class="description ">
+              <p
+                className="padding-top"
+              >
                 Working as an Intern I was tasked with Quality Assurance responsibilities including writing test suites for their frontend using Cypress.io. 
                 While writing test cases if bugs were discovered I was responsible for drafting up a new bug ticket in Jira. Along with being tasked to
                 implement fixes for the bugs that I encountered. 
@@ -34,13 +63,15 @@ const ProfessionalExperience = () => {
             </div>
           </div>
         </div>
+
         <div className="item">
-          <div style={{background: 'black', minHeight: '100px'}} className="image">
-            <img style={{top: "50%", transform: "translateY(25%)", padding:"5px", justifyContent: 'center' }} 
+          <div 
+          className="ui small image experience-img theorem">
+            <img 
              src={'https://theoremadvertising.com/wp-content/uploads/2021/01/xcropped-Theorem-Logo-White-Text-2.png.pagespeed.ic.Ie6RP6MNgW.webp'}
               alt="Theorem Advertising" />
           </div>
-          <div className="content" style={{ paddingTop: "10px" }}>
+          <div className="middle aligned content">
             <StyledA
               target="_blank"
               rel="noopener noreferrer"
@@ -49,7 +80,8 @@ const ProfessionalExperience = () => {
             >
               Theorem Advertising 
             </StyledA>
-            <div style={{ paddingLeft: "5px" }} class="ui header">August 2020 - October 2020</div>
+            <div 
+            class="ui header padding-left">August 2020 - October 2020</div>
             
             <div class="description">
             <StyledA
@@ -61,7 +93,6 @@ const ProfessionalExperience = () => {
               Detailed Resume 
             </StyledA>
               <p
-               style={{ paddingTop: "10px" }}
               >
                 Working as an Intern I was responsible for improving & adding features for their client's websites. 
                 Helped implement a new component for the Portland Trail Blazers, a live court play by play page with in game updates and a shot map of where shots where made and missed.
@@ -70,9 +101,9 @@ const ProfessionalExperience = () => {
               </p>
              
             </div>
-          </div>
+          </div> 
         </div>
-      </div>
+        </div>
     </PaddedContainer>
   );
 };
