6a693a18f64adce4f281a8a2f305fa4d3033015c
jhell85 <jhell85@gmail.com>
2021-06-24 14:13:03 -0700

    updated descriptions of projects

--- src/PersonalProjects.js
+++ src/PersonalProjects.js
@@ -15,7 +15,7 @@ const PersonalProjects = () => {
           <div className="image">
             <img src={wordGame} alt="GIF of Generic Word Game" />
           </div>
-          <div className="content" style={{ paddingTop: "10px" }}>
+          <div className="middle aligned content" >
             <a 
               target="_blank"
               rel="noopener noreferrer"
@@ -26,9 +26,7 @@ const PersonalProjects = () => {
             </a>
             <div class="description">
               <p>
-                a hangman style word game that is built with ReactJS and uses Redux for
-                State management, It has a very simple but effective UI. This was one of
-                my first apps I built using ReactJS and Redux.{" "}
+              A Hangman style word game with a very simple but effective UI, built using ReactJS and Redux for State management. This was my first project using ReactJS and Redux.
               </p>
             </div>
           </div>
@@ -37,7 +35,7 @@ const PersonalProjects = () => {
           <div className="image">
             <img src={wagerPlacer} alt="GIF of Wager Placer" />
           </div>
-          <div className="content" style={{ paddingTop: "10px" }}>
+          <div className="middle aligned content" style={{ paddingTop: "10px" }}>
             <a
               target="_blank"
               rel="noopener noreferrer"
@@ -48,10 +46,7 @@ const PersonalProjects = () => {
             </a>
             <div class="description">
               <p>
-                a social media app designed to let users place wagers on sporting events
-                with one another, this app is still in it's early stages of development.
-                The app is developed with a MERN stack, uses Redux for managing State and
-                React-router for app navigation.
+              A social media app designed to allow users to place wagers with one another on sporting events. This app is still in its infant stages of development, however it is built with a MERN stack and utilizes Redux for managing State and React-router for app navigation
               </p>
             </div>
           </div>
@@ -60,7 +55,7 @@ const PersonalProjects = () => {
           <div className="image">
             <img src={streamers} alt="GIF of Streamers"/>
           </div>
-          <div className="content" style={{ paddingTop: "10px" }}>
+          <div className="middle aligned content" style={{ paddingTop: "10px" }}>
             <a
               target="_blank"
               rel="noopener noreferrer"
@@ -71,9 +66,7 @@ const PersonalProjects = () => {
             </a>
             <div class="description">
               <p>
-                an app that lets users start their own stream and broadcast it. the app
-                has Google OAuth2 for user authentication and has full CRUD for records of
-                streams.
+              An app that allows users to start their own stream and broadcast it. This app utilizes Google OAuth2 for user authentication and has full CRUD for records of streams.
               </p>
             </div>
           </div>
