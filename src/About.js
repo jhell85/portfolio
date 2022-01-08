e95eef5fc90df111d375b69b1bec1931f5618f65
jhell85 <jhell85@gmail.com>
2022-01-08 14:05:16 -0800

    Improved spacing of items, replaced blackjack.gif, removed un-used css classes

--- src/About.js
+++ src/About.js
@@ -6,29 +6,23 @@ const About = () => {
   return (
     <Container className="ui items padded-container">
       <div className="item">
-        <div className="image">
+        <div className="ui medium rounded image">
           <img src={headShot} alt="Head Shot of Josh" />
         </div>
         <div className=" middle aligned content">
           <div className="header">
             <h3>
-              My name is Josh and I'm a web developer located in Portland, Oregon. I've
-              been programming and developing web apps since 2018 using a variety of
-              languages and frameworks. I'm passionate and enthusiastic about developing
-              intuitive interfaces that facilitate a superior user experience as well as
-              creating applications and web services to help others learn.{" "}
+              My name is Josh and I'm a web developer located in Portland, Oregon. I've been programming and
+              developing web apps since 2018 using a variety of languages and frameworks. I'm passionate and
+              enthusiastic about developing intuitive interfaces that facilitate a superior user experience as
+              well as creating applications and web services to help others learn.{" "}
               <div>
-                Prior to web development, I spent several years in the US Navy station on
-                board the{" "}
-                <a
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  href="https://en.wikipedia.org/wiki/USS_Chosin"
-                >
+                Prior to web development, I spent several years in the US Navy station on board the{" "}
+                <a target="_blank" rel="noopener noreferrer" href="https://en.wikipedia.org/wiki/USS_Chosin">
                   USS Chosin
                 </a>
-                , once honorably discharged I went on to own and operate an HVAC
-                construction company out of Gilbert, Arizona.
+                , once honorably discharged I went on to own and operate an HVAC construction company out of
+                Gilbert, Arizona.
               </div>
             </h3>
           </div>
